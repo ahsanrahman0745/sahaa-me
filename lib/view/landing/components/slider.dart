c499b50d1d13038d9fa241ea544b6439db1ef9c0
TAYYAB SHAH <tayyabshah010@gmail.com>
2022-09-15 14:17:11 +0500

    update-banner-search-page

--- lib/view/landing/components/slider.dart
+++ lib/view/landing/components/slider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:sahaame/view/landing/components/waveclipper.dart';
 
 import '../../../responsive.dart';
